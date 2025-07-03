c10c3b71609aa7b6b8b216459445406fdbe0e2e1
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-07-03 17:01:49 +0100

    Add all Controls

---
+++ src/main/java/com/nathan/springsecurity/controller/CardsController.java
@@ -0,0 +1,12 @@
+package com.nathan.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+    @GetMapping("/welcome")
+    public String welcomeMessage() {
+        return "Welcome to Spring Security with security REST API!";
+    }
+}
