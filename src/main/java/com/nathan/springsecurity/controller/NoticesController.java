7591bf495d53d13947fab953ed616bcfbb059bce
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-07-21 16:39:10 +0100

    update userDetailsService class to create to create users with password but not encoded yet. leverage the InMemoryUserDetailsManager

--- src/main/java/com/nathan/springsecurity/controller/NoticesController.java
+++ src/main/java/com/nathan/springsecurity/controller/NoticesController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NoticesController {
-    @GetMapping("/Notices")
+    @GetMapping("/notices")
     public String getNoticesDetails() {
         return "Here are the Notices details from the DB!";
     }
