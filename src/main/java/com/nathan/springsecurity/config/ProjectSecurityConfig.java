f7985f6c9e2cff0bacd6ad1d5c580e696136e992
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-07-21 16:58:46 +0100

    update the ProjectSecurityConfig to make use of a PasswordEncoder which gonna leverage PassowordEncoderFactories to make use to bcrypt to hash the password

--- src/main/java/com/nathan/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/nathan/springsecurity/config/ProjectSecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -42,8 +45,14 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}12345").roles("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").roles("admin").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$TVv1Q86DjrbwWz4Wk7bLseHmGP.4vsYjrxdjF3vS52brTnpcGTUNa").roles("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
 
