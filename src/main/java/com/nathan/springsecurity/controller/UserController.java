78600b642e7f463e3088fe0c78e4dd48adc8b110
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-07-23 17:05:30 +0100

    add REST api to register a user in database

---
+++ src/main/java/com/nathan/springsecurity/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.nathan.springsecurity.controller;
+
+import com.nathan.springsecurity.model.Customer;
+import com.nathan.springsecurity.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+
+            Customer savedcustomer = customerRepository.save(customer);
+
+            if (savedcustomer.getId() > 0) {
+                return ResponseEntity.status(HttpStatus.CREATED)
+                        .body("Given UserDetails are successfully registered");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("User Registration Failed");
+            }
+
+        }catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured" + e.getMessage());
+
+        }
+    }
+}
