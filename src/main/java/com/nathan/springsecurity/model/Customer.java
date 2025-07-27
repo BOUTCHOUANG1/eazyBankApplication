a04517fd967a10ad7a75baa38bcbf6ebfff21e46
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-07-27 02:18:57 +0100

    update models for project

--- src/main/java/com/nathan/springsecurity/model/Customer.java
+++ src/main/java/com/nathan/springsecurity/model/Customer.java
@@ -1,8 +1,12 @@
 package com.nathan.springsecurity.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.sql.Date;
 
 @Entity
 @Getter
@@ -11,9 +15,22 @@ import lombok.Setter;
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
     private long id;
+
+    private String name;
+
     private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String pwd;
-   // @Column(name = "role")
+
     private String role;
+
+    @Column(name = "create_dt")
+    @JsonIgnore
+    private Date createDt;
 }
