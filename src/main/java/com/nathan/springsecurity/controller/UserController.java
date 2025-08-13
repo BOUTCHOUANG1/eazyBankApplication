72886930a02f846dd3e751e11332e0329f289cce
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-08-13 14:52:19 +0100

    feat: improve user management logic in UserController

--- src/main/java/com/nathan/springsecurity/controller/UserController.java
+++ src/main/java/com/nathan/springsecurity/controller/UserController.java
@@ -1,63 +1,45 @@
 package com.nathan.springsecurity.controller;
 
-import com.nathan.springsecurity.constants.ApplicationConstants;
 import com.nathan.springsecurity.model.Customer;
-import com.nathan.springsecurity.model.LoginRequestDTO;
-import com.nathan.springsecurity.model.LoginResponseDTO;
 import com.nathan.springsecurity.repository.CustomerRepository;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.sql.Date;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final CustomerRepository customerRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final Environment env;
+//    private final PasswordEncoder passwordEncoder;
+//    private final AuthenticationManager authenticationManager;
+    // private final Environment env;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
-        try {
-            String hashPwd = passwordEncoder.encode(customer.getPwd());
-            customer.setPwd(hashPwd);
-            customer.setCreateDt(new Date(System.currentTimeMillis()));
-            Customer savedcustomer = customerRepository.save(customer);
-
-            if (savedcustomer.getId() > 0) {
-                return ResponseEntity.status(HttpStatus.CREATED)
-                        .body("Given UserDetails are successfully registered");
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body("User Registration Failed");
-            }
-
-        }catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception occurred" + e.getMessage());
-
-        }
-    }
+//    @PostMapping("/register")
+//    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+//        try {
+//            String hashPwd = passwordEncoder.encode(customer.getPwd());
+//            customer.setPwd(hashPwd);
+//            customer.setCreateDt(new Date(System.currentTimeMillis()));
+//            Customer savedcustomer = customerRepository.save(customer);
+//
+//            if (savedcustomer.getId() > 0) {
+//                return ResponseEntity.status(HttpStatus.CREATED)
+//                        .body("Given UserDetails are successfully registered");
+//            } else {
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//                        .body("User Registration Failed");
+//            }
+//
+//        }catch(Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("An exception occurred" + e.getMessage());
+//
+//        }
+//    }
 
     @RequestMapping("/user")
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
@@ -65,27 +47,27 @@ public class UserController {
         return optionalCustomer.orElse(null);
     }
 
-    @PostMapping("/apiLogin")
-    public ResponseEntity<LoginResponseDTO> apiLogin (@RequestBody LoginRequestDTO loginRequest) {
-        String jwt = "";
-        Authentication authentication = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(),
-                loginRequest.password());
-        Authentication authenticationResponse = authenticationManager.authenticate(authentication);
-        if(null != authenticationResponse && authenticationResponse.isAuthenticated()) {
-            if (null != env) {
-                String secret = env.getProperty(ApplicationConstants.JWT_SECRET,
-                        ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
-                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-                jwt = Jwts.builder().issuer("Eazy Bank").subject("JWT Token")
-                        .claim("username", authenticationResponse.getName())
-                        .claim("authorities", authenticationResponse.getAuthorities().stream().map(
-                                GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
-                        .issuedAt(new java.util.Date())
-                        .expiration(new java.util.Date((new java.util.Date()).getTime() + 30000000))
-                        .signWith(secretKey).compact();
-            }
-        }
-        return ResponseEntity.status(HttpStatus.OK).header(ApplicationConstants.JWT_HEADER,jwt)
-                .body(new LoginResponseDTO(HttpStatus.OK.getReasonPhrase(), jwt));
-    }
+//    @PostMapping("/apiLogin")
+//    public ResponseEntity<LoginResponseDTO> apiLogin (@RequestBody LoginRequestDTO loginRequest) {
+//        String jwt = "";
+//        Authentication authentication = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(),
+//                loginRequest.password());
+//        Authentication authenticationResponse = authenticationManager.authenticate(authentication);
+//        if(null != authenticationResponse && authenticationResponse.isAuthenticated()) {
+//            if (null != env) {
+//                String secret = env.getProperty(ApplicationConstants.JWT_SECRET,
+//                        ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+//                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+//                jwt = Jwts.builder().issuer("Eazy Bank").subject("JWT Token")
+//                        .claim("username", authenticationResponse.getName())
+//                        .claim("authorities", authenticationResponse.getAuthorities().stream().map(
+//                                GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
+//                        .issuedAt(new java.util.Date())
+//                        .expiration(new java.util.Date((new java.util.Date()).getTime() + 30000000))
+//                        .signWith(secretKey).compact();
+//            }
+//        }
+//        return ResponseEntity.status(HttpStatus.OK).header(ApplicationConstants.JWT_HEADER,jwt)
+//                .body(new LoginResponseDTO(HttpStatus.OK.getReasonPhrase(), jwt));
+//    }
 }
